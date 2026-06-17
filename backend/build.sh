51bd7f11d92548bc72aedab594776fb9d2d47c70
Sparsh Agarwal <98206877+sparshag09@users.noreply.github.com>
2026-06-18 02:55:12 +0530

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,6 +1,21 @@
+##!/usr/bin/env bash
+#set -o errexit
+
+#pip install -r requirements.txt
+#python manage.py collectstatic --no-input
+#python manage.py migrate
 #!/usr/bin/env bash
 set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+
+# Create superuser non-interactively using environment variables
+python manage.py createsuperuser --no-input \
+  --username "$DJANGO_SUPERUSER_USERNAME" \
+  --email "$DJANGO_SUPERUSER_EMAIL" \
+  --password "$DJANGO_SUPERUSER_PASSWORD"
+
+# Migrate database and collect static files
 python manage.py migrate
+python manage.py collectstatic --no-input   
